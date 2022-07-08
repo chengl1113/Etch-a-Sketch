8fcbaf2e33898827d8b7ce2c7ea1f7203975d372
cheng <chengl@bu.edu>
2022-07-07 21:57:55 -0400

    implement method allowing users to choose number of rows

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+let rows = prompt("Enter the number of squares per row: (max 100)");
+
+
+
 const container = document.querySelector('#container');
-let rows = 16;
-let cols = 16;
+let containerWidth = container.clientWidth;
+let containerHeight = container.clientHeight;
 for (let i = 0; i < rows; i++){
-    for (let j = 0; j < cols; j++){
+    for (let j = 0; j < rows; j++){
         const content = document.createElement('div');
         content.classList.add('square');
+        content.style.maxWidth = `${containerWidth/rows}px`;
+        content.style.maxHeight = `${containerHeight/rows}px`;
         container.appendChild(content);
     }
 }
+let gridTemplate = '';
+for (let i = 0; i < rows; i++){
+    gridTemplate += '1fr ';
+}
+
+container.style.gridTemplateColumns = gridTemplate;
+container.style.gridTemplateRows = gridTemplate;
 
 const squares = document.querySelectorAll('.square');
 squares.forEach((square) => {
